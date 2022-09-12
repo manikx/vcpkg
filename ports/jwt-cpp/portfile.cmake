e89b8e79bedbac24fd2fcfbbbe92cfec9d6a6c37
Bajpai, ManikX <manikx.bajpai@intel.com>
2022-09-12 17:17:21 +0530

    Fix jwt-cpp for cfc.pral.ms.authentication
    A jwt specific patch was added to vcpkg tree. This is in line with:-
    "https://github.com/intel-sandbox/cfc.pral.build/blob/release_branch
    /jwtcpp/0001-Add-set_scope-API.patch"
    
    JiRA Id:- "https://jira.devtools.intel.com/browse/LNUX-2876"
    
    Reported-by: Bajpai, ManikX <manikx.bajpai@intel.com>
    Signed-off-by: Bajpai, ManikX <manikx.bajpai@intel.com>

--- ports/jwt-cpp/portfile.cmake
+++ ports/jwt-cpp/portfile.cmake
@@ -4,8 +4,9 @@ vcpkg_from_github(
     REF 4a537e969891dde542ad8b1a4a214955a83be29f # v0.6.0
     SHA512 eeeb6adb7f94b699a020b5622b9dbb6c677d92779b57bfb2298b331a5cf69d9112d0b123f0c2ca235ecd96df6d32fcf44e85e144fa414aeff8fd67e3b87576d2
     HEAD_REF master
+    PATCHES
+        Add-set_scope-API.patch
 )
-
 # Copy the header files
 file(GLOB HEADER_FILES ${SOURCE_PATH}/include/jwt-cpp/*)
 file(COPY ${HEADER_FILES} DESTINATION ${CURRENT_PACKAGES_DIR}/include/jwt-cpp)
